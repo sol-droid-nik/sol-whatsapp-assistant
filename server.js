3cd42f4226962b5c57f11e439873fee171cfe4b1
sol-droid-nik <2nik1siz@gmail.com>
2025-11-18 12:46:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.json({ limit: "10mb" }));
 // ===== CONFIG =====
 const VERIFY_TOKEN = process.env.WHATSAPP_VERIFY_TOKEN;
 const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN;
+const WHATSAPP_NUMBER_ID = process.env.WHATSAPP_NUMBER_ID;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const KB_FILES = "./kb"; // папка с файлами SOL (мы подключим позже)
 
